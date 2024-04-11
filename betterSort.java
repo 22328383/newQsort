d883ecdfa61e3a84bb06737c40658e13cfa332a3
22328383 <andrei.krivoruchko@ucdconnect.ie>
2024-04-11 18:50:06 +0100

    code finished, added test with built-in timer, fixed errors in proposed code pseudocode

--- betterSort.java
+++ betterSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class betterSort {
 
     public static int[] betterQuickSort(int[] array, int low, int high) {
@@ -5,7 +7,7 @@ public class betterSort {
         if(N <= 3) {
             return manSort(array, low, high);
         } else {
-            int pivot = calcPivot(array, low, high);
+            double pivot = calcPivot(array, low, high);
             int q     = partition(array, low, high, pivot);
             betterQuickSort(array, low, q);
             betterQuickSort(array, q+1, high);
@@ -13,20 +15,46 @@ public class betterSort {
         return array;
     }
 
-    private static int partition(int[] array, int low, int high, int pivot) {
-        // TODO
-        return 0;
+    private static int partition(int[] array, int low, int high, double pivot) {
+        int i = low - 1;
+        int j = high + 1;
+        while (true) {
+            do {
+                i++;
+            } while (array[i] < pivot);
+            do {
+                j--;
+            } while (array[j] > pivot);
+            if (i < j) {
+                swap(array, i, j);
+            } else {
+                return j;
+            }
+        }
     }
 
-    public static int calcPivot(int[] array, int low, int high) {
-        int mid = ((low + high)/2);
-        int min1 = findMin(array, low, mid);
-        int min2 = findMin(array, mid, high);
-        int max1 = findMax(array, low, mid);
-        int max2 = findMax(array, mid, high);
+    public static double calcPivot(int[] array, int low, int high) {
+//        double piv;
+//
+//        int mid = ((low + high)/2);
+//        int min1 = findMin(array, low, mid);
+//        int min2 = findMin(array, mid + 1, high);
+//        int max1 = findMax(array, low, mid);
+//        int max2 = findMax(array, mid + 1, high);
+//
+//        piv = ((double)((max1 + min1) / (double)(mid - low + 1) + (max2 + min2) / (double)(high - mid))) / 2;
+//
+//
+//        ^^^ this may lead to a pivot smaller than all elements => infinite loop of partitioning
+//        (Example : [88 77 66 55 44 99 110 121], ( (88+55)/4 + (121+44)/4) )/2 = 38.5)
 
-        int result = (min1 + min2 + max1 + max2) >> 3;
-        return result;
+        int sum = 0;
+        for(int i = low; i <= high; i++) {
+            sum = sum + array[i];
+        }
+
+        double piv = (double) sum / (high-low+1);
+        return piv;
     }
 
     private static int[] manSort(int[] array, int low, int high) {
@@ -62,7 +90,7 @@ public class betterSort {
 
     private static int findMin(int[] array, int low, int high) {
         int min = array[low];
-        for(int i = low; i < high; i++) {
+        for(int i = low; i <= high; i++) {
             int current = array[i];
             if(current < min) {
                 min = current;
@@ -73,7 +101,7 @@ public class betterSort {
 
     private static int findMax(int[] array, int low, int high) {
         int max = array[low];
-        for(int i = low; i < high; i++) {
+        for(int i = low; i <= high; i++) {
             int current = array[i];
             if(current > max) {
                 max = current;
--- betterSortTest.java
+++ betterSortTest.java
@@ -1,23 +1,88 @@
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class betterSortTest {
 
     @Test
-    public void manualSort() {
+    public void manualSortTest() {
         int[] array1 = {3, 1, 2};
         int[] expected1 = {1, 2, 3};
 
         betterSort.betterQuickSort(array1, 0, 2);
         assertArrayEquals(expected1, array1);
-
     }
 
     @Test
-    public void calcPivot() {
+    public void calcPivotTest() {
         int[] array1 = {88,77,66,55,44,33,22,11};
+        int[] array2 = {11,22,33,44,55,66,77,88};
+        int[] array3 = {66,55,44,33,77,88};
 
-        assertEquals(24, betterSort.calcPivot(array1, 0, array1.length));
+        int[] input1 = new int[25];
+        for(int i = 25; i > 1; i--) {
+            input1[25-i] = (i*11);
+        }
+
+        assertEquals(49.5, betterSort.calcPivot(array1, 0, array1.length-1), 0.15);
+
+        assertEquals(49.5, betterSort.calcPivot(array2, 0, array2.length-1), 0.15);
+
+        assertEquals(60.5, betterSort.calcPivot(array3, 0, array3.length-1), 0.15);
+
+        assertEquals(142.5, betterSort.calcPivot(input1, 0, input1.length-1), 0.15);
+    }
+
+    @Test
+    public void betterQuickSortTest() {
+        int[] array1 = {121,110,99,88,77,66,55,44,33,22,11};
+        int[] array2 = {11,22,33,44,55,66,77,88,99,110,121};
+
+        assertArrayEquals(array2, betterSort.betterQuickSort(array1, 0, array1.length-1));
+        System.out.println(Arrays.toString(betterSort.betterQuickSort(array1, 0, array1.length - 1)));
+    }
+
+    @Test
+    public void speedTest25() {
+        int[] input1 = new int[25];
+        for(int i = 25; i > 1; i--) {
+            input1[25-i] = (i*11);
+        }
+
+        long startTime = System.nanoTime();
+        betterSort.betterQuickSort(input1, 0, input1.length-1);
+        long endTime = System.nanoTime();
+        long elapsedTime = (endTime - startTime);
+        System.out.println("Elapsed time: " + elapsedTime + " nanoseconds");
+    }
+
+    @Test
+    public void speedTest50() {
+        int[] input1 = new int[50];
+        for(int i = 50; i > 1; i--) {
+            input1[50-i] = (i*11);
+        }
+
+        long startTime = System.nanoTime();
+        betterSort.betterQuickSort(input1, 0, input1.length-1);
+        long endTime = System.nanoTime();
+        long elapsedTime = (endTime - startTime);
+        System.out.println("Elapsed time: " + elapsedTime + " nanoseconds");
+    }
+
+    @Test
+    public void speedTest100() {
+        int[] input1 = new int[100];
+        for(int i = 100; i > 1; i--) {
+            input1[100-i] = (i*11);
+        }
+
+        long startTime = System.nanoTime();
+        betterSort.betterQuickSort(input1, 0, input1.length-1);
+        long endTime = System.nanoTime();
+        long elapsedTime = (endTime - startTime);
+        System.out.println("Elapsed time: " + elapsedTime + " nanoseconds");
     }
 }
